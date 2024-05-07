dcf4899aff4fed69b67ac2bfee5c3f63a3f62b9a
herculesvanso <vansohercules@gmail.com>
2024-05-06 21:11:59 -0300

    Update

--- machinelearning.py
+++ machinelearning.py
@@ -32,6 +32,19 @@ class Mlp():
     def __init__(self,eta=.2,maxit=100) -> None:
         self.eta = eta
         self.maxit = maxit
+
+    def fit(self,inputs,targets):
+        n = inputs.shape[1]
+        m = targets.shape[1]
+        l = 2
+        inputs_bias = np.concatenate((inputs,np.array(inputs.shape[0]*[-1]).reshape(-1,1)),axis=1)
+        weights_inputs_bias = np.random.rand(l,n+1)
+        weights_outhidden_bias = np.random.rand(m,l+1)
+        for i in range(self.maxit):
+            out_hidden = sigmoid(np.dot(weights_inputs_bias,inputs_bias[i])).reshape(-1,1)
+            out_hidden_bias = np.concatenate((out_hidden,np.array([-1]).reshape(-1,1)),axis=0)
+            out = sigmoid(np.dot(weights_outhidden_bias,out_hidden_bias))
+
     
     #to continue...
     #testing commiting in vscode
